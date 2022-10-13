20914d3cab6308cbd637703b1bc1f51f9df20d3c
cyccc95 <cyccc95@naver.com>
2022-10-13 18:02:15 +0900

    map, useState

---
+++ app03/src/components/MyCom3.js
@@ -0,0 +1,42 @@
+import MyStyle3 from "./MyStyle3.css";
+import { useState } from "react";
+
+function MyCom3(props){
+  const myMv = {...props.item};
+  
+  // map() 함수
+  // let arr = [1,2,3]
+  // arr = arr.map((item) => item * 2)
+  // console.log(arr)
+
+  // const lis = Object.entries(myMv).map((item) => (
+  //     <li key={item}  className='mvLi'>
+  //       <span className="mvLiK">{item[0]}</span>
+  //       <span className="mvLiV">{item[1]}</span>
+  //     </li>
+  // ))
+  const keys = ['rank', 'movieNm'];
+  let lis = keys.map((k) =>
+        <div key={k + myMv['movieCd']} className={k}>{myMv[k]}</div>
+  )  
+
+  let [count, upCount] = useState(0);
+  
+  return(
+    <div className="container">
+      <div className="box">
+        <li className="mvLi">
+          {lis}
+        </li>
+      </div>
+      <div className="like">
+        <span onClick={() => {
+          upCount(count+1)
+          }}>❤</span>
+        <span>{count}</span>
+      </div>
+    </div>
+  );
+}
+
+export default MyCom3;
